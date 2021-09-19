edd54fe6ce69d0b618b8c011945b19f427048a31
SawitriFK <sawitrifinak@gmail.com>
2021-09-19 19:30:07 +0700

    Upload one

---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGameOver : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // reload
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
